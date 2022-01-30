39756f78c1d652f78b1c2659f5fa694d983cb153
utk1106 <thaokarutkarsh@gmail.com>
2022-01-30 17:28:01 +0530

    Add files via upload

---
+++ Assignment-3.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Don't go Outside in Odd day
+
+# In[4]:
+
+
+numbers = (1,2,3,4,5,6,7,8,9)
+even_no =0
+odd_no=0
+for x in numbers:
+    if x % 2 == 0:
+        even_no+=1
+    else:
+        odd_no+=1
+print("Number of even numbers :",even_no)
+print("Number of odd numbers :",odd_no)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
